cb7d6be1cfb50ff2b04d0f0a125a209ef7e85498
mengzhaoxiang <mengzhaoxiang@xd.com>
2020-10-12 11:04:37 +0800

    1

--- gridpathfinder/Assets/Editor/PathFinderEditorWindow.cs
+++ gridpathfinder/Assets/Editor/PathFinderEditorWindow.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
 public class PathFinderEditorWindow : EditorWindow
 {
+    LayerMask unwalkableMask = new LayerMask();
+    Vector2 gridWorldSize = new Vector2();
+    float nodeRadius;
+
     [MenuItem("Tools/寻路")]
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(PathFinderEditorWindow), false, "寻路", true);
+    }
+    private static void DrawUILine(int thickness = 1, int padding = 10)
+    {
+        Color color = Color.gray;
+        Rect r = EditorGUILayout.GetControlRect(GUILayout.Height(padding + thickness));
+        r.height = thickness;
+        r.y += padding / 2;
+        r.x -= 2;
+        r.width += 6;
+        EditorGUI.DrawRect(r, color);
     }
     private void OnGUI()
     {
@@ -21,6 +36,29 @@ public class PathFinderEditorWindow : EditorWindow
             GUILayout.FlexibleSpace();
             GUILayout.Label("寻路");
             GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+        }
+        DrawUILine();
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("不可行走:");
+            LayerMask tempMask = EditorGUILayout.MaskField(InternalEditorUtility.LayerMaskToConcatenatedLayersMask(unwalkableMask), InternalEditorUtility.layers);
+            unwalkableMask = InternalEditorUtility.ConcatenatedLayersMaskToLayerMask(tempMask);
+            GUILayout.EndHorizontal();
+        }
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("区域大小:");
+            gridWorldSize = EditorGUILayout.Vector2Field("", gridWorldSize);
+            GUILayout.EndHorizontal();
+        }
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.Label("方格半径:");
+            nodeRadius = EditorGUILayout.FloatField(nodeRadius);
             GUILayout.EndHorizontal();
         }
         {
@@ -37,6 +75,7 @@ public class PathFinderEditorWindow : EditorWindow
             GUILayout.FlexibleSpace();
             GUILayout.EndHorizontal();
         }
+        DrawUILine();
         GUILayout.EndVertical();
     }
 }
