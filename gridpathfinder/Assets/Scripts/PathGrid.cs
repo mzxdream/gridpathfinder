7adb7c7ce6bd495c20cf9a27a3489eaf128d2a18
mzxdream <mzxdream@gmail.com>
2020-10-22 12:02:42 +0800

    1

---
+++ gridpathfinder/Assets/Scripts/PathGrid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+public class PathNode
+{
+    public Vector3 position;
+    public int x;
+    public int y;
+    public bool walkable;
+    public int gCost;
+    public int hCost;
+    public PathNode parent;
+    public PathNode(Vector3 position, int x, int y, bool walkable)
+    {
+        this.position = position;
+        this.x = x;
+        this.y = y;
+        this.walkable = walkable;
+    }
+    public int FCost { get { return gCost + hCost; } }
+}
+
+public class PathGrid
+{
+    Vector3 originPosition;
+    int width;
+    int height;
+    float cellSize;
+    PathNode[,] nodes;
+    public PathGrid(Vector3 originPosition, int width, int height, float cellSize)
+    {
+        this.originPosition = originPosition;
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        nodes = new PathNode[width, height];
+        for (int x = 0; x < nodes.GetLength(0); x++)
+        {
+            for (int y = 0; y < nodes.GetLength(1); y++)
+            {
+                Vector3 pos = originPosition + new Vector3(x + 0.5f, 0f, y + 0.5f) * cellSize;
+                nodes[x, y] = new PathNode(pos, x, y, true);
+            }
+        }
+    }
+    public int Width { get => width; }
+    public int Height { get => height; }
+    public PathNode GetNode(int x, int y)
+    {
+        return nodes[x, y];
+    }
+    public PathNode GetNodeFromPos(Vector3 worldPosition)
+    {
+        int x = Mathf.Clamp(Mathf.FloorToInt((worldPosition.x - originPosition.x) / cellSize), 0, width - 1);
+        int y = Mathf.Clamp(Mathf.FloorToInt((worldPosition.z - originPosition.z) / cellSize), 0, height - 1);
+        return nodes[x, y];
+    }
+}
---
+++ gridpathfinder/Assets/Scripts/Testing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float cellSize;
+    [SerializeField] private GameObject walkablePrefab;
+    [SerializeField] private GameObject unwalkablePrefab;
+    PathGrid grid;
+    GameObject[,] gridObjs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new PathGrid(gameObject.transform.position, width, height, cellSize);
+        gridObjs = new GameObject[width, height];
+        for (int x = 0; x < grid.Width; x++)
+        {
+            for (int y = 0; y < grid.Height; y++)
+            {
+                var node = grid.GetNode(x, y);
+                var go = Instantiate(walkablePrefab, null, false);
+                go.transform.position = node.position;
+                go.transform.localScale = go.transform.localScale * cellSize;
+                gridObjs[x, y] = go;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            {
+                var pos = hit.point;
+                var node = grid.GetNodeFromPos(pos);
+                node.walkable = !node.walkable;
+                Destroy(gridObjs[node.x, node.y]);
+                if (node.walkable)
+                {
+                    var go = Instantiate(walkablePrefab, null, false);
+                    go.transform.position = node.position;
+                    go.transform.localScale = go.transform.localScale * cellSize;
+                    gridObjs[node.x, node.y] = go;
+                }
+                else
+                {
+                    var go = Instantiate(unwalkablePrefab, null, false);
+                    go.transform.position = node.position;
+                    go.transform.localScale = go.transform.localScale * cellSize;
+                    gridObjs[node.x, node.y] = go;
+                }
+            }
+        }
+    }
+}
