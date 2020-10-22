bdd5d118804f121e0725d6705fa1dabecb53d4f7
mzxdream <mzxdream@gmail.com>
2020-10-22 18:40:56 +0800

    1

---
+++ gridpathfinder/Assets/Scripts/PathManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PathManager
+{
+    static PathManager instance;
+    private PathManager() { }
+
+    public static PathManager Instance()
+    {
+        if (instance == null)
+        {
+            instance = new PathManager();
+        }
+        return instance;
+    }
+
+    public void DeletePath(int pathID)
+    {
+    }
+    public bool IsFinalized()
+    {
+        return true;
+    }
+    public int RequestPath(Unit obj, Vector3 sourcePoint, Vector3 targetPoint, float radius, bool synced)
+    {
+        if (!IsFinalized())
+        {
+            return 0;
+        }
+        return 0;
+    }
+    public bool UpdatePath(Unit owner, int pathID)
+    {
+        return true;
+    }
+    public Vector3 NextWayPoint(Unit obj, int pathID, int numRetries, Vector3 point, float radius, bool synced)
+    {
+        return Vector3.zero;
+    }
+}
---
+++ gridpathfinder/Assets/Scripts/PathMathUtils.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PathMathUtils
+{
+    public static float SqrDistance2D(Vector3 pos1, Vector3 pos2)
+    {
+        var x = pos1.x - pos2.x;
+        var z = pos1.z - pos2.z;
+        return x * x + z * z;
+    }
+}
