7c255124fb6040c711f8f53cb51f5d31a80fc78f
mengzhaoxiang <mengzhaoxiang@xd.com>
2020-11-02 18:50:47 +0800

    1

--- gridpathfinder/Assets/Scripts/game.cs
+++ gridpathfinder/Assets/Scripts/game.cs
@@ -2,9 +2,9 @@
 
 public class Game
 {
-    public const int GAME_SPEED = 30;
-    public const int SQUARE_SIZE = 1;
-
+    public const int GAME_SPEED = 30; // 每秒多少帧
+    public const int SQUARE_SIZE = 8; // 1米8elmos
+    public const int CIRCLE_DIVS = 32768 * 2; //一圈多少 2PI
     public static int frameNum = 1;
     public static void Update()
     {
